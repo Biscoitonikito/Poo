744ff2d66611bf46c0a55554817a3d347395980c
GUilherme Sousa <guilhermeplay300@gmail.com>
2019-01-02 17:09:48 -0200

    Trabalho engComputação

---
+++ teste/app/src/main/java/com/example/guilherme/teste/App.java
@@ -0,0 +1,36 @@
+package com.example.guilherme.teste;
+
+import android.app.Application;
+import android.util.Log;
+
+import io.objectbox.BoxStore;
+import io.objectbox.android.AndroidObjectBrowser;
+import com.example.guilherme.teste.App;
+import com.example.guilherme.teste.R;
+/**
+ * Created by rogermac on 22/01/18.
+ */
+
+public class App extends Application {
+
+    public static final String TAG = "ObjectBoxExample";
+    public static final boolean EXTERNAL_DIR = false;
+
+    private BoxStore boxStore;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        boxStore = MyObjectBox.builder().androidContext(App.this).build();
+
+        if (BuildConfig.DEBUG) {
+            new AndroidObjectBrowser(boxStore).start(this);
+        }
+
+        Log.d("App", "Using ObjectBox " + BoxStore.getVersion() + " (" + BoxStore.getVersionNative() + ")");
+    }
+
+    public BoxStore getBoxStore() {
+        return boxStore;
+    }
+}
